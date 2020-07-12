01dbea1a25f50f338f3faf244a1053c7a14532f9
50BraulioAlvarez <braulioalvarezurban@gmail.com>
2020-07-12 16:00:15 -0500

    practicas cinta roja

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className='navbar navbar-light bg_primary'>
+            <a className='navbar_brand text-light' href='#'>Navbar</a>
+        </nav>
+    )
+}
+
+export default Navbar;
---
+++ src/components/Searchinput.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Searchinput = () => {
+    return (
+        <div className="input-group input-group-lg mt-3 mb-3">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="inputGroup-sizing-lg">Buscar GIF</span>
+            </div>
+            <input type="text" class="form-control" />
+        </div>
+
+    )
+}
+
+export default Searchinput
